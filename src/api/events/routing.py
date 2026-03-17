21f7d5e044bfb45360d37dbc4ea0ace019607bab
satvik18 <satvikramanujam@gmail.com>
2026-03-17 10:22:45 +0530

    adding sqlmodel timescaledb

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter
-from .schemas import EventSchema
-
+from .schemas import EventSchema, EventCreateSchema, EventUpdateSchema
+import os
+from api.db.config import DATABASE_URL
 app = APIRouter()
 
-@app.get('/')
-def get_information():
-    return {
-        "items":[1,2,3,4]
-    }
+@app.get("/")
+def get_love() -> EventSchema:
+    print(os.environ.get("DATABASE_URL"), DATABASE_URL)
+    return {"id":69}
 
-@app.get("/{ids}")
-def get_ids(ids) -> EventSchema:
-    return {"id":ids}
+@app.post("/")
+def give_love(payload:EventCreateSchema) -> EventSchema:
+    return {"id":payload.id}
+
+@app.put("/{id}")
+def update_love(payload:EventUpdateSchema) -> EventSchema:
+    return {"id":payload.id}
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,4 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import List, Optional
 
 class EventSchema(BaseModel):
     id: int
+    page: Optional[str] = Field(default='')
+    description:Optional[str] = Field(default='')
+
+class EventCreateSchema(BaseModel):
+    id:int
+    page: List[str]
+    description:Optional[str] = Field(default='')
+
+class EventUpdateSchema(BaseModel):
+    id: int
+    page: Optional[str] = Field(default='')
+    description: str
+    
